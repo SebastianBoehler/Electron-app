38ec000acb5a4249afa33c5113d96b8693200b66
Sebastian_B <basti.boehler@hotmail.de>
2020-05-31 12:50:19 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+
+let mainWindow
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        hasShadow: false,
+        //autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: 'loop supreme'
+    })
+
+    mainWindow.loadFile('renderer.html')
+})
+
+// receive message from index.html 
+ipcMain.on('item:add', (event, arg) => {
+    console.log(event)
+    console.log(arg)
+    mainWindow.webContents.send('item:add', arg)
+})
---
+++ renderer.js
@@ -0,0 +1,2 @@
+console.log('renderer.js executed')
+
